c337681bf9a9d87ac4d4f290aad004e88f99a176
aimperial <Andres.Imperial@sdl.usu.edu>
2020-02-06 18:01:51 -0700

    HW4 finished

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <algorithm>
+#include <cmath>
 #include <cstdio>
 #include <mpi.h>
 #include <time.h>
@@ -7,98 +7,81 @@
 using namespace std;
 #define MCW MPI_COMM_WORLD
 
+pair<bool, int> compareLow(int myIdx, int j, int myVal) {
+  int myPartner = myIdx ^ (1 << j);
+  MPI_Send(&myVal, 1, MPI_INT, myPartner, 0, MCW);
+  int compVal;
+  MPI_Recv(&compVal, 1, MPI_INT, myPartner, 0, MCW, MPI_STATUS_IGNORE);
+  return make_pair(compVal < myVal, compVal);
+}
+
+pair<bool, int> compareHigh(int myIdx, int j, int myVal) {
+  auto result = compareLow(myIdx, j, myVal);
+  return make_pair(!result.first, result.second);
+}
+
 int main(int argc, char **argv) {
   int rank, size;
   int data;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MCW, &rank);
   MPI_Comm_size(MCW, &size);
+  auto dimensions = log2(size);
+  int myVal;
+  std::vector<int> totalArray = {};
 
   if (rank == 0) {
+    // create random array sized < 100
     srand(time(0));
-    unsigned int numElements = rand() % 100;
-    vector<int> unsortedArray(numElements);
-
-    unsigned int rowsPerThread = numElements / size;
-
-
-    for (int i = 0; i < numElements; i++) {
-      unsortedArray[i] = rand() % 100;
+    // Populate array with random int < 100
+    for (int i = 0; i < size; i++) {
+      totalArray.push_back(rand() % 100);
     }
 
-    for (int process = 1; process < size; ++process) {
-      unsigned int start = process * rowsPerThread + 1;
-      unsigned int end = (process + 1) * rowsPerThread;
-
-      if (numElements - end < rowsPerThread)
-        end = numElements - 1;
-
-      int elements = end - start + 1;
-
-      printf("start: %u -- end: %u\n", start, end);
-
-      MPI_Send(&elements, 1, MPI_INT, process, 0, MCW);
-
-      MPI_Send(&unsortedArray[start], elements, MPI_INT, process, 0, MCW);
+    myVal = totalArray[0];
+    for (int i = 1; i < size; i++) {
+      MPI_Send(&totalArray[i], 1, MPI_INT, i, 0, MCW);
     }
+  } else {
+    MPI_Recv(&myVal, 1, MPI_INT, 0, 0, MCW, MPI_STATUS_IGNORE);
+  }
 
-    vector<vector<int>> sortedArrays;
-
-    vector<int> myArray(unsortedArray.begin(),
-                        unsortedArray.begin() + rowsPerThread);
-    sort(myArray.begin(), myArray.end());
-    sortedArrays.push_back(myArray);
-    for (int process = 1; process < size; ++process) {
-      unsigned int start = process * rowsPerThread + 1;
-      unsigned int end = (process + 1) * rowsPerThread;
-
-      if (numElements - end < rowsPerThread)
-        end = numElements - 1;
-
-      int elements = end - start + 1;
-
-      vector<int> temp(elements);
-      MPI_Recv(&temp[0], elements, MPI_INT, MPI_ANY_SOURCE, 1, MCW,
-               MPI_STATUS_IGNORE);
-      sortedArrays.push_back(temp);
-    }
-
-    vector<int> finalArray(numElements);
-    for (unsigned int i = 0; i < numElements; ++i) {
-      int smallest = 0;
-      for (unsigned int j = 1; j < sortedArrays.size(); ++j) {
-        if (sortedArrays[smallest] > sortedArrays[j]) {
-          smallest = j;
+  {
+    for (int i = 0; i < dimensions; i++) {
+      for (int j = i; j >= 0; j--) {
+        if (((rank >> (i + 1)) % 2 == 0 && (rank >> j) % 2 == 0) ||
+            ((rank >> (i + 1)) % 2 != 0 && (rank >> j) % 2 != 0)) {
+          auto result = compareLow(rank, j, myVal);
+          if (result.first) {
+            myVal = result.second;
+          }
+        } else {
+          auto result = compareHigh(rank, j, myVal);
+          if (result.first) {
+            myVal = result.second;
+          }
         }
       }
-      finalArray[i] = sortedArrays[smallest][0];
-      printf("smallest: %i -- sortedArrays.size() : %lu\n", smallest,
-             sortedArrays.size());
-      sortedArrays[smallest].erase(sortedArrays[smallest].begin());
-      if (sortedArrays[smallest].empty()) {
-        sortedArrays.erase(sortedArrays.begin() + smallest);
-      }
-    }
-
-    for (auto element : finalArray) {
-      printf("%i\n", element);
     }
   }
 
-  else {
-    int elements;
-    printf("here\n");
-    MPI_Recv(&elements, 1, MPI_INT, 0, 0, MCW, MPI_STATUS_IGNORE);
+  if (rank == 0) {
+    totalArray[0] = myVal;
+    for (int i = 1; i < size; i++) {
+      int temp;
+      MPI_Recv(&temp, 1, MPI_INT, i, 0, MCW, MPI_STATUS_IGNORE);
+      totalArray[i] = temp;
+    }
+  } else {
+    MPI_Send(&myVal, 1, MPI_INT, 0, 0, MCW);
+  }
 
-    printf("elements: %i\n", elements);
-    vector<int> myArray(elements, 0);
-    MPI_Recv(&myArray[0], elements, MPI_INT, 0, 0, MCW, MPI_STATUS_IGNORE);
-    printf("here\n");
-
-    sort(myArray.begin(), myArray.end());
-    printf("here\n");
-
-    MPI_Send(&myArray[0], elements, MPI_INT, 0, 1, MCW);
+  if (rank == 0) {
+    printf("totalArray:");
+    for (int i = 0; i < size; i++) {
+      printf("%i,", totalArray[i]);
+    }
+    printf("\n");
   }
 
   MPI_Finalize();

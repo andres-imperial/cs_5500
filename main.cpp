98e4ac185b6421c26b289d8a00660d9a6eaafea5
Dersy <dersy@pop-os.localdomain>
2020-03-12 21:02:17 -0600

    HW6 done

--- main.cpp
+++ main.cpp
@@ -4,48 +4,55 @@
 #include <cstdio>
 #include <fstream>
 #include <mpi.h>
+#include <time.h>
 #include <vector>
 
 using namespace std;
 #define MCW MPI_COMM_WORLD
+int HEIGHT = 20;
+int WIDTH = 40;
 
-int WIDTH = 800;
-int HEIGHT = 800;
-float re0;
-float re1;
-float c0;
-float c1;
-
-int value(int x, int y) {
-  complex<float> point((static_cast<float>(x) * (re0 - re1) / WIDTH) + re1,
-                       (static_cast<float>(y) * (c0 - c1) / HEIGHT) + c1);
-
-  complex<float> z(point);
-  unsigned int numIters = 0;
-  for (; abs(z) < 2 && numIters <= 512; ++numIters) {
-    z = z * z + point;
+int getNeighbor(int row, int col, vector<int> &board) {
+  // use modulus to get wrapping effect at board edges
+  if (row < 0 || row >= HEIGHT) {
+    return 0;
   }
+  return board[((row + HEIGHT) % HEIGHT) * WIDTH + ((col + WIDTH) % WIDTH)];
+}
 
-  if (numIters < 34) {
-    return 255 * numIters / 33;
+int getCount(int row, int col, vector<int> &board) {
+  int count = 0;
+  std::vector<int> deltas{-1, 0, 1};
+  for (int dc : deltas) {
+    for (int dr : deltas) {
+      if (dr || dc) {
+        count += getNeighbor(row + dr, col + dc, board);
+      }
+    }
   }
+  return count;
+}
 
-  return 0;
+int tick(vector<int> &board, int row, int col) {
+  int count = getCount(row, col, board);
+  bool birth = !board[row * WIDTH + col] && count == 3;
+  bool survive = board[row * WIDTH + col] && (count == 2 || count == 3);
+  return birth || survive;
+}
+
+void showCell(int cell) { std::cout << (cell ? "*" : " "); }
+
+void showCells(vector<int> &board) {
+  for (int row = 0; row < HEIGHT; ++row) {
+    std::cout << "|";
+    for (int col = 0; col < WIDTH; ++col) {
+      showCell(board[row * WIDTH + col]);
+    }
+    std::cout << "|\n";
+  }
 }
 
 int main(int argc, char **argv) {
-  if (argc != 4) {
-    re0 = 0.0014;
-    re1 = -0.748;
-    c0 = 0.1;
-    c1 = c0 + (re1 - re0);
-  } else {
-    re0 = atoi(argv[1]);
-    c0 = atoi(argv[2]);
-    re1 = atoi(argv[3]);
-    c1 = c0 + (re1 - re0);
-  }
-
   int rank, size;
   int data;
   MPI_Init(&argc, &argv);
@@ -53,31 +60,72 @@ int main(int argc, char **argv) {
   MPI_Comm_size(MCW, &size);
 
   auto start = std::chrono::high_resolution_clock::now();
+  std::vector<int> allRows(HEIGHT * WIDTH, 0);
 
   int numRows = HEIGHT / size;
   if (rank < static_cast<int>(HEIGHT) % size) {
     ++numRows;
   }
 
-  std::vector<int> subRows(numRows * WIDTH);
-
-  for (int row = 0; row < numRows; ++row) {
-    for (int col = 0; col < WIDTH; ++col) {
-      int val = value(col, row * size + rank);
-      subRows[row * WIDTH + col] = val;
+  if (rank == 0) {
+    srand(time(NULL));
+    for (int row = 0; row < HEIGHT; ++row) {
+      for (int col = 0; col < WIDTH; ++col) {
+        for (auto &&val : allRows) {
+          val = rand() % 2;
+        }
+      }
     }
+    showCells(allRows);
   }
 
-  if (rank == 0) {
-    ofstream myBrot("./myBrot.ppm");
+  int numGens = 100;
+  for (int genCount = 0; genCount < numGens; ++genCount) {
+    if (rank == 0) {
+      // The rows they need to compute plus border rows above and below if
+      // applicable
+      auto start = numRows * WIDTH;
+      for (int i = 1; i < size; ++i) {
+        int tempRows = HEIGHT / size;
+        if (i < static_cast<int>(HEIGHT) % size) {
+          ++tempRows;
+        }
 
-    // Image header
-    myBrot << "P3\n" << WIDTH << " " << HEIGHT << " 255\n";
+        auto dataSize = (tempRows + 2) * WIDTH;
+        if (i == size - 1) {
+          dataSize = (tempRows + 1) * WIDTH;
+        }
+        MPI_Send(&allRows[start - (1 * WIDTH)], dataSize, MPI_INT, i, 0, MCW);
+        start += (tempRows * WIDTH);
+      }
+    } else {
+      // Recieve data
+      auto dataSize = (numRows + 2) * WIDTH;
+      if (rank == size - 1) {
+        dataSize = (numRows + 1) * WIDTH;
+      }
+      std::vector<int> temp(dataSize, 0);
+      MPI_Recv(&temp[0], dataSize, MPI_INT, 0, 0, MCW, MPI_STATUS_IGNORE);
+      auto original = temp;
+      for (int row = 1; row < numRows + 1; ++row) {
+        for (int col = 0; col < WIDTH; ++col) {
+          temp[row * WIDTH + col] = tick(original, row, col);
+        }
+      }
+      // Send data to thread 0
+      MPI_Send(&temp[WIDTH], numRows * WIDTH, MPI_INT, 0, 0, MCW);
+    }
 
-    if (myBrot.good()) {
+    if (rank == 0) {
+      std::vector<int> myWork(numRows * WIDTH);
+      for (int row = 0; row < numRows; ++row) {
+        for (int col = 0; col < WIDTH; ++col) {
+          myWork[row * WIDTH + col] = tick(allRows, row, col);
+        }
+      }
       // Recieve data
       vector<vector<int>> finalRows(size, vector<int>{});
-      finalRows[0] = subRows;
+      finalRows[0] = myWork;
 
       for (int i = 1; i < size; ++i) {
         int tempRows = HEIGHT / size;
@@ -90,40 +138,17 @@ int main(int argc, char **argv) {
                  MPI_STATUS_IGNORE);
         finalRows[i] = temp;
       }
-
-      for (int row = 0; row < HEIGHT; ++row) {
-        for (int col = 0; col < WIDTH; ++col) {
-          auto val = finalRows[row % size][row / size * WIDTH + col];
-          switch (row % 6) {
-          case 0:
-            myBrot << val << ' ' << 0 << ' ' << 0 << "\n";
-            break;
-          case 1:
-            myBrot << val << ' ' << val << ' ' << 0 << "\n";
-            break;
-          case 2:
-            myBrot << 0 << ' ' << val << ' ' << 0 << "\n";
-            break;
-          case 3:
-            myBrot << 0 << ' ' << val << ' ' << val << "\n";
-            break;
-          case 4:
-            myBrot << 0 << ' ' << 0 << ' ' << val << "\n";
-            break;
-          case 5:
-            myBrot << val << ' ' << 0 << ' ' << val << "\n";
-            break;
-          };
+      // put data from final rows to all rows
+      allRows.clear();
+      for (int i = 0; i < size; ++i) {
+        for (auto val : finalRows[i]) {
+          allRows.push_back(val);
         }
       }
-
-      myBrot.close();
+      showCells(allRows);
+      cout << endl;
     } else {
-      throw std::runtime_error("was not able to open file");
     }
-  } else {
-    // Send data to thread 0
-    MPI_Send(&subRows[0], numRows * WIDTH, MPI_INT, 0, 0, MCW);
   }
 
   MPI_Finalize();
@@ -132,7 +157,7 @@ int main(int argc, char **argv) {
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration =
         std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
-    printf("%li\n", duration.count());
+    printf("Duration: %li\n", duration.count());
   }
 
   return 0;
